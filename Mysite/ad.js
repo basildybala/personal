e0802bc47c0fa963069008a0b67c27da3c03975e
basildybala <akbasil123@gmail.com>
2021-12-23 14:10:32 +0530

    ad

--- Mysite/ad.js
+++ Mysite/ad.js
@@ -1,4 +1,11 @@
+
 //LAZY LOAD
 const myLazyLoad= new LazyLoad ({
     elements_selector:".lazy-photo"
 });
+
+//Search Google
+function searchit() {
+document.getElementById("hp").href=(("https://www.google.com/search?q=site:dramalite.com ")+
+(document.getElementById("linkit").value));
+}
