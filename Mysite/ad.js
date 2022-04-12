ca8a843602573e20c2216fb10a3e8aa551f8529e
basildybala <akbasil123@gmail.com>
2022-04-12 14:18:49 +0530

    asdASD

--- Mysite/ad.js
+++ Mysite/ad.js
@@ -54,49 +54,78 @@
 
 
 
-const ul = document.querySelector("ul"),
-input = document.querySelector(".content input"),
-tagNumb = document.querySelector(".details span");
-let maxTags = 10,
-tags = ["coding", "kerala"];
-countTags();
-createTag();
-function countTags(){
-    input.focus();
-    tagNumb.innerText = maxTags - tags.length;
+// const ul = document.querySelector("ul"),
+// input = document.querySelector(".content input"),
+// tagNumb = document.querySelector(".details span");
+// let maxTags = 10,
+// tags = ["coding", "kerala"];
+// countTags();
+// createTag();
+// function countTags(){
+//     input.focus();
+//     tagNumb.innerText = maxTags - tags.length;
+// }
+// function createTag(){
+//     ul.querySelectorAll("li").forEach(li => li.remove());
+//     tags.slice().reverse().forEach(tag =>{
+//         let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+//         ul.insertAdjacentHTML("afterbegin", liTag);
+//     });
+//     countTags();
+// }
+// function remove(element, tag){
+//     let index  = tags.indexOf(tag);
+//     tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+//     element.parentElement.remove();
+//     countTags();
+// }
+// function addTag(e){
+//     if(e.key == "Enter"){
+//         let tag = e.target.value.replace(/\s+/g, ' ');
+//         if(tag.length > 1 && !tags.includes(tag)){
+//             if(tags.length < 10){
+//                 tag.split(',').forEach(tag => {
+//                     tags.push(tag);
+//                     createTag();
+//                 });
+//             }
+//         }
+//         e.target.value = "";
+//     }
+// }
+// input.addEventListener("keyup", addTag);
+// const removeBtn = document.querySelector(".details button");
+// removeBtn.addEventListener("click", () =>{
+//     tags.length = 0;
+//     ul.querySelectorAll("li").forEach(li => li.remove());
+//     countTags();
+// });
+
+
+// variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+// variables for navbar search toggle
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+
+// navbar menu toggle function
+function navIsActive() {
+  header.classList.toggle('active');
+  nav.classList.toggle('active');
+  navbarMenuBtn.classList.toggle('active');
 }
-function createTag(){
-    ul.querySelectorAll("li").forEach(li => li.remove());
-    tags.slice().reverse().forEach(tag =>{
-        let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
-        ul.insertAdjacentHTML("afterbegin", liTag);
-    });
-    countTags();
-}
-function remove(element, tag){
-    let index  = tags.indexOf(tag);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    element.parentElement.remove();
-    countTags();
-}
-function addTag(e){
-    if(e.key == "Enter"){
-        let tag = e.target.value.replace(/\s+/g, ' ');
-        if(tag.length > 1 && !tags.includes(tag)){
-            if(tags.length < 10){
-                tag.split(',').forEach(tag => {
-                    tags.push(tag);
-                    createTag();
-                });
-            }
-        }
-        e.target.value = "";
-    }
-}
-input.addEventListener("keyup", addTag);
-const removeBtn = document.querySelector(".details button");
-removeBtn.addEventListener("click", () =>{
-    tags.length = 0;
-    ul.querySelectorAll("li").forEach(li => li.remove());
-    countTags();
-});
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+
+// navbar search toggle function
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
